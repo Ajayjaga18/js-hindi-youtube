bd7b4c0e48c26867d77e6e39ad832acdd684c7d0
Ajayjaga18 <105097335+Ajayjaga18@users.noreply.github.com>
2025-02-25 11:31:11 +0000

    variavles agin

--- 01_basic/variables.js
+++ 01_basic/variables.js
@@ -1,1 +1,6 @@
-console.log("variable");
+let name = "ajauy kumar jaga";
+let email = "ajayjaga1111@gmail.com";
+let account;
+console.table([name,email,account]);
+
+//prefer not to use var bacaucse of issue var
